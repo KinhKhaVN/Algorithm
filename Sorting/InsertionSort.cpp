0be1222801eb855c828bd3aa5168d1eaf35ba997
KinhKhaVN <pebin0123@gmail.com>
2026-04-12 20:28:55 +0700

    Add private sort function

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -2,25 +2,30 @@
 #include "Utility.h"
 #include <cstdio>
 
-
 void InsertionSort::Sort(Data data, Compare compare)
 {
-  size_t size = data.n;
   Data newData = data;
 
-  for (size_t i = 0; i < size - 1; i++)
+  insertionSort(newData, compare);
+
+  newData.Print();
+}
+
+void InsertionSort::insertionSort(Data &data, Compare compare)
+{
+
+  for (size_t i = 0; i < data.n - 1; i++)
   {
     size_t j = i + 1;
-    int tmp = newData[j];
-    while ( !compare(tmp, newData[j - 1]) &&
+    int tmp = data[j];
+    while ( !compare(tmp, data[j - 1]) &&
             j > 0) 
     {
-      newData[j] = newData[j - 1];
+      data[j] = data[j - 1];
       j--;
     }
 
     if (j >= 0 )
-      newData[j] = tmp;
+      data[j] = tmp;
   }
-  newData.Print();
 }
--- Sorting/InsertionSort.h
+++ Sorting/InsertionSort.h
@@ -7,6 +7,8 @@ class InsertionSort: public SortStragety
   public:
     InsertionSort(): SortStragety("Insertion Sort") {}
     void Sort(Data data, Compare compare) override;
+  private:
+    void insertionSort(Data &data, Compare compare);
 };
 
 
