a8d3073823f5c6fcd4c3fd7b0679a4ae92793763
KinhKhaVN <pebin0123@gmail.com>
2026-04-12 20:30:13 +0700

    Add compare function and private sort function

--- Sorting/HeapSort.cpp
+++ Sorting/HeapSort.cpp
@@ -4,40 +4,40 @@ void HeapSort::Sort(Data data, Compare compare)
 {
   Data newData = data;
 
-  heapSort(newData, data.n - 1);
+  heapSort(newData, data.n - 1, compare);
 
   newData.Print();
 }
 
-void HeapSort::Heapify(Data &data, size_t index, size_t size)
+void HeapSort::Heapify(Data &data, size_t index, size_t size, Compare compare)
 {
   size_t k = index;
   size_t left = ( index << 1 ) + 1;
   size_t right = left + 1;
 
   if ( (left < size) &&
-       (data[left] < data[k]) )
+       (compare( data[left], data[k]))  )
     k = left;
   
   if ( (right < size) &&
-            (data[right] < data[k]) )
+        (compare( data[right], data[k])) )
     k = right;
 
   if ( k != index )
   {
     std::swap( data[index], data[k] );
-    Heapify(data, k, size);
+    Heapify(data, k, size, compare);
   }
 }
 
-void HeapSort::heapSort(Data &data, size_t size)
+void HeapSort::heapSort(Data &data, size_t size, Compare compare)
 {
-  for (long long i = (size >> 1); i >= 0; i--)
-    Heapify(data, (size_t)i, size);
+  for (long long i = (size / 2); i >= 0; i--)
+    Heapify(data, (size_t)i, size, compare);
 
   for (long long i = size; i > 0; i--)
   {
     std::swap(data[0], data[i]);
-    Heapify(data, 0, (size_t)i);
+    Heapify(data, 0, (size_t)i, compare);
   }
 }
--- Sorting/HeapSort.h
+++ Sorting/HeapSort.h
@@ -7,9 +7,9 @@ class HeapSort: public SortStragety
   public:
     HeapSort(): SortStragety("Heap Sort") {}
     virtual void Sort(Data data, Compare compare) override;
-  public:
-    void Heapify(Data &data, size_t index, size_t size);
-    void heapSort(Data &data, size_t size);
+  private:
+    void Heapify(Data &data, size_t index, size_t size, Compare compare);
+    void heapSort(Data &data, size_t size, Compare compare);
 };
 
 
