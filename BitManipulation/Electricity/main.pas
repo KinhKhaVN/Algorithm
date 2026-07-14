e21fb5da1d4f75f49f5a507f73edccf4fbbcf246
KinhKhaVN <pebin0123@gmail.com>
2026-07-15 04:00:22 +0700

    Mạch nối tiếp
    - Mạch nối tiếp được biểu diễn thông qua các trạng thái bit
    - Sử dụng 2 mạch X, Y chứa 2 trạng thái bit {0, 1}, ta có được 2^2 trạng
    thái
    - 1 <= i, j <= 2
    - R sẽ nhận kết quả của X[i] AND Y[j], ta mô hình hóa dòng điện bằng
    cách R chỉ có dòng điện khi dòng điện chạy qua mạch X[i] và Y[j]

--- BitManipulation/Electricity/main.pas
+++ BitManipulation/Electricity/main.pas
@@ -3,16 +3,32 @@ PROGRAM Electricity;
 
 USES crt;
 
+TYPE
+  TrangThaiBit = array [1..2] of boolean;
 
+VAR
+  X: TrangThaiBit = (true, false);
+  Y: TrangThaiBit = (true, false);
+  R: boolean;
 
+  i: integer;
+  j: integer;
 
+PROCEDURE 
+  MachNoiTiep(VAR X, Y: TrangThaiBit; VAR R: boolean);
+BEGIN
+  Writeln('----------Mach Noi Tiep---------');
 
-
+  FOR i:= 1 to 2 DO
+    FOR j := 1 to 2 DO
+      BEGIN
+        R := X[i] AND Y[j];
+        Writeln('X: ', X[i], ', Y: ', Y[j], ', R = ', R);
+      END;
+END;
 
 BEGIN
-  
-  CLRSCR;
 
-  Writeln('Hello World');
+  MachNoiTiep(X, Y, R);
 
 END.
