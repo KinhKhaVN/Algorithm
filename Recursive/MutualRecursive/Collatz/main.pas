a0d8ba0d1f7553eca8ae5be9dcd7c7bfde3503e4
KinhKhaVN <pebin0123@gmail.com>
2026-08-03 11:36:42 +0700

    Mutual recursive
    - The main function contain two conponent function
    - Each component function call the main function

--- Recursive/MutualRecursive/Collatz/main.pas
+++ Recursive/MutualRecursive/Collatz/main.pas
@@ -5,6 +5,22 @@ USES crt;
 VAR
   n: integer = 10;
 
+PROCEDURE Solve(n: integer); forward;
+
+PROCEDURE
+  SolveEven(n: integer);
+  BEGIN
+    Write(n, ' div 2 = ');
+    Solve(n div 2)
+  END;
+
+PROCEDURE
+  SolveOdd(n: integer);
+  BEGIN
+    Write(n, ' *3 + 1 = ');
+    Solve(3*n + 1);
+  END;
+
 PROCEDURE
   Solve(n: integer);
   BEGIN
@@ -18,16 +34,13 @@ PROCEDURE
 
     if (n mod 2) = 0 then 
       BEGIN
-        Write(n, ' div 2 = ');
-        Solve(n div 2)
+        SolveEven(n)
       END
     else 
       BEGIN
-        Write(n, ' *3 + 1 = ');
-        Solve(3*n + 1);
+        SolveOdd(n);
       END;
   END;
-
 
 BEGIN
   CLRSCR;
