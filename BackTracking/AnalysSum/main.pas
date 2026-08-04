831ed6faf05113139181f99e351a7333bee760f2
KinhKhaVN <pebin0123@gmail.com>
2026-08-04 17:03:02 +0700

    Optimize space complexity
    - Use two array current[0..n], next[0..n]
    - current is row 1 and next is row 2
    - current is row 2 and next is row 3
    - ... until end
    - During solving, only line k and k + 1 is important

--- BackTracking/AnalysSum/main.pas
+++ BackTracking/AnalysSum/main.pas
@@ -6,10 +6,10 @@ CONST
   n = 5;
 VAR
   dp: array[0..n, 0..n] of integer;
-  i: integer;
+  current, next: array[0..n] of integer;
 
 PROCEDURE
-  Init;
+  Init1;
   VAR
     i: integer;
   BEGIN
@@ -20,7 +20,7 @@ PROCEDURE
   END;
 
 PROCEDURE
-  Print;
+  Print1;
   VAR
     i, j: integer;
   BEGIN
@@ -30,10 +30,11 @@ PROCEDURE
           Write(dp[i][j]:3);
         Writeln;
       END;
+    Writeln;
   END;
 
 FUNCTION
-  Solve: integer;
+  Solve1: integer;
   VAR
     i, j: integer;
   BEGIN
@@ -47,11 +48,53 @@ FUNCTION
       END;
     exit(dp[n][n]);
   END;
-  
+ 
+PROCEDURE
+  Init2;
+  VAR
+    i: integer;
+  BEGIN
+    FillChar(current, SizeOf(current), 0);
+    current[0] := 1;
+  END;
+
+FUNCTION
+  Solve2:integer;
+  VAR
+    i, j: integer;
+  BEGIN
+    for i := 1 to n do
+      BEGIN
+        for j := 0 to n do
+          BEGIN
+            if i > j then next[j] := current[j]
+            else next[j] := current[j] + next[j - i];
+          END;
+        current := next;
+      END;
+  END;
+
+PROCEDURE
+  Print2;
+  VAR
+    i: integer;
+  BEGIN
+    for i := 0 to n do
+      Write(current[i]:3);
+    Writeln;
+    for i := 0 to n do
+      Write(next[i]:3);
+    Writeln;
+  END;
+
 BEGIN
 
-  Init;
-  Solve;
-  Print;
+  Init1;
+  Solve1;
+  Print1;
+
+  Init2;
+  Solve2;
+  Print2;
 
 END.
