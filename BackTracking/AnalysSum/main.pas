a76f87ae50b88cbbaeddea8c27137472e387b7a6
KinhKhaVN <pebin0123@gmail.com>
2026-08-05 14:16:57 +0700

    Using 2D array but swap role between 2 rows; using 1 array to solve
    - Solve4 uses 1 array to solve
    - Time: O(n^2)
    - Space: O(n)

--- BackTracking/AnalysSum/main.pas
+++ BackTracking/AnalysSum/main.pas
@@ -4,23 +4,30 @@ USES crt;
 
 CONST
   n = 5;
+
+TYPE
+  Array2D = array of array of integer;
+
 VAR
-  dp: array[0..n, 0..n] of integer;
+  dp: Array2D;
   current, next: array[0..n] of integer;
+  L: array[0..n] of integer;
+  B: array[1..2, 0..n] of integer;
+  x, y: integer;
 
 PROCEDURE
-  Init1;
+  Init(VAR dp: Array2D);
   VAR
     i: integer;
   BEGIN
-    FillChar(dp[0], SizeOf(dp[0]), 0);
+    SetLength(dp, n + 1, n + 1);
     dp[0][0] := 1;
     for i := 0 to n do
       dp[i][0] := 1;
   END;
 
 PROCEDURE
-  Print1;
+  Print(CONST dp: Array2D);
   VAR
     i, j: integer;
   BEGIN
@@ -34,7 +41,7 @@ PROCEDURE
   END;
 
 FUNCTION
-  Solve1: integer;
+  Solve(VAR dp: Array2D): integer;
   VAR
     i, j: integer;
   BEGIN
@@ -48,20 +55,43 @@ FUNCTION
       END;
     exit(dp[n][n]);
   END;
- 
+
 PROCEDURE
-  Init2;
+  Print(VAR current, next: array of integer);
   VAR
     i: integer;
   BEGIN
-    FillChar(current, SizeOf(current), 0);
-    current[0] := 1;
+    for i := 0 to n do
+      Write(current[i]:3);
+    Writeln;
+    for i := 0 to n do
+      Write(next[i]:3);
+    Writeln;
+  Writeln
+  END;
+
+PROCEDURE
+  Print(VAR dp: array of integer);
+  VAR
+    i: integer;
+  BEGIN
+    for i := 0 to n do
+      Write(dp[i]:3);
+    Writeln;
+  Writeln
+  END;
+
+PROCEDURE
+  Init(VAR dp: array of integer);
+  BEGIN
+    FillChar(dp, SizeOf(dp), 0);
+    dp[0] := 1;
   END;
 
 FUNCTION
-  Solve2:integer;
+  Solve(VAR current, next: array of integer): integer;
   VAR
-    i, j: integer;
+    i, j, k: integer;
   BEGIN
     for i := 1 to n do
       BEGIN
@@ -70,8 +100,19 @@ FUNCTION
             if i > j then next[j] := current[j]
             else next[j] := current[j] + next[j - i];
           END;
-        current := next;
+          for k := 0 to n do current[k] := next[k];
       END;
+    exit(current[n]);
+  END;
+
+FUNCTION
+  Solve(VAR dp: array of integer): integer;
+  VAR
+    i, j: integer;
+  BEGIN
+    for i := 1 to n do
+        for j := i to n do dp[j] := dp[j] + dp[j - i];
+    exit(dp[n]);
   END;
 
 PROCEDURE
@@ -85,16 +126,73 @@ PROCEDURE
     for i := 0 to n do
       Write(next[i]:3);
     Writeln;
+  Writeln
   END;
+
+PROCEDURE
+  Init3;
+  BEGIN
+    FillChar(B[1], SizeOf(B[1]), 0);
+    B[1][0] := 1;
+    x := 1;
+    y := 2;
+  END;
+
+FUNCTION
+  Solve3:integer;
+  VAR
+    i, j: integer;
+  BEGIN
+    for i := 1 to n do
+      BEGIN
+        for j := 0 to n do
+          BEGIN
+            if i > j then B[y][j] := B[x][j]
+            else B[y][j] := B[x][j] + B[y][j - i];
+          END;
+          x := 3 - x;
+          y := 3 - y;
+      END;
+    exit(B[x][n]);
+  END;
+
+PROCEDURE
+  Print3;
+  VAR
+    i, j: integer;
+  BEGIN
+    for i := 1 to 2 do
+      BEGIN
+        for j := 0 to n do
+          Write(B[i][j]:3);
+    Writeln;
+      END;
+    Writeln
+  END;
+
+
 
 BEGIN
 
-  Init1;
-  Solve1;
-  Print1;
+  Writeln('Solve1');
+  Init(dp);
+  Solve(dp);
+  Print(dp);
 
-  Init2;
-  Solve2;
-  Print2;
+  Writeln('Solve2');
+  Init(current);
+  Solve(current, next);
+  Print(current);
+  Print(next);
+
+  Writeln('Solve3');
+  Init3;
+  Solve3;
+  Print3;
+
+  Writeln('Solve4');
+  Init(L);
+  Solve(L);
+  Print(L);
 
 END.
