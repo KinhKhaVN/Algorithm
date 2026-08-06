d927cf0cbeefe1af217001a668db6c2d27f9eb71
KinhKhaVN <pebin0123@gmail.com>
2026-08-06 14:44:32 +0700

    Minumum cost for 1 to k steps at each stair

--- BackTracking/StaircaseWithMinimumCost/main.pas
+++ BackTracking/StaircaseWithMinimumCost/main.pas
@@ -26,16 +26,32 @@ FUNCTION
   END;
 
 FUNCTION
-  Solve(VAR dp, cost: array of integer): integer;
+  Solve_From_1_To_2_Steps(VAR dp, cost: array of integer): integer;
   VAR
     i: integer;
   BEGIN
+    Init;
     for i := 2 to n do
-      dp[i] := min(dp[i - 1], dp[i - 2]) + cost[i];
+      dp[i] := Min(dp[i - 1], dp[i - 2]) + cost[i];
 
     exit(dp[n]);
   END;
  
+FUNCTION
+  Solve_From_1_To_K_Steps(VAR dp, cost: array of integer; CONST k: integer): integer;
+  VAR
+    i, j: integer;
+  BEGIN
+    Init;
+    for i := 2 to n do
+      BEGIN
+        for j := 1 to k do
+          BEGIN
+            if i - j >= 0 then dp[i] := Min(dp[i], dp[i - j] + cost[i]);
+          END;
+      END;
+  END;
+
 PROCEDURE
   Print(CONST X: array of integer);
   BEGIN
@@ -46,14 +62,22 @@ PROCEDURE
 
 BEGIN
 
-  Init;
-  Solve(dp, cost);
+  Solve_From_1_To_2_Steps(dp, cost);
 
+  Writeln('-----From 1 to 2 steps-----');
   Writeln('Cost');
   Print(cost);
+  Writeln;
+  Writeln('Dp');
+  Print(dp);
 
   Writeln;
 
+  Solve_From_1_To_K_Steps(dp, cost, 2);
+  Writeln('-----From 1 to k steps-----');
+  Writeln('Cost');
+  Print(cost);
+  Writeln;
   Writeln('Dp');
   Print(dp);
 END.
